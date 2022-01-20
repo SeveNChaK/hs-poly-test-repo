436d7040bce39d0a92b99297a0ec5ae3b0303b0a
Alex Korneev <korneev.ry@yandex.ru>
2022-01-20 13:38:07 +0300

    part3 без grokBy

--- src/Part3/Tasks.hs
+++ src/Part3/Tasks.hs
@@ -1,22 +1,32 @@
 module Part3.Tasks where
 
+import Data.List
+import Data.Function
 import Util (notImplementedYet)
 
 -- Функция finc принимает на вход функцию f и число n и возвращает список чисел [f(n), f(n + 1), ...]
 finc :: (Int -> a) -> Int -> [a]
-finc = notImplementedYet
+finc f n = f n : finc f (n + 1)
 
 -- Функция ff принимает на вход функцию f и элемент x и возвращает список [x, f(x), f(f(x)), f(f(f(x))) ...]
 ff :: (a -> a) -> a -> [a]
-ff = notImplementedYet
+ff f x = x : ff f (f x)
 
 -- Дан список чисел. Вернуть самую часто встречающуюся *цифру* в этих числах (если таковых несколько -- вернуть любую)
 mostFreq :: [Int] -> Int
-mostFreq = notImplementedYet
+mostFreq numbers = last $ maximumBy (compare `on` length) $ group $ sort $ concatMap digitals numbers
+
+digitals :: Integral x => x -> [x]
+digitals 0 = []
+digitals x = x `mod` 10 : digitals (x `div` 10)
 
 -- Дан список lst. Вернуть список элементов из lst без повторений, порядок может быть произвольным.
 uniq :: (Eq a) => [a] -> [a]
-uniq = notImplementedYet
+uniq = rdHelper []
+           where rdHelper seen [] = seen
+                 rdHelper seen (x:xs)
+                     | x `elem` seen = rdHelper seen xs
+                     | otherwise = rdHelper (seen ++ [x]) xs
 
 -- Функция grokBy принимает на вход список Lst и функцию F и каждому возможному
 -- значению результата применения F к элементам Lst ставит в соответствие список элементов Lst,
